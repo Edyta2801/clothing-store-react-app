d7267c76b53f4831f03bf7e2a58c4a012892f575
Edyta2801 <edyta.szarowska@interia.pl>
2021-03-16 23:25:43 +0100

    replace component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,14 +7,16 @@ import {  Hero,
 //  import {Carousel} from '../../components/Carousel';
 //  import {Box, Button, Flex, Image, Heading, Stack, Text} from '@chakra-ui/react';
  import {Services} from '../../components/Services';
-import {ProductCard} from '../../components/ProductCard';
+// import {ProductCard} from '../../components/ProductCard';
+import {FeaturedProducts} from '../../components/FeaturedProducts';
+
 
 const Home = () => {
   return (
     <main>
       <Hero />
-      <ProductCard/>
-      {/* <FeaturedProducts /> */}
+      {/* <ProductCard/> */}
+      <FeaturedProducts />
       <Services />
       {/* <Contact /> */}
     </main>
