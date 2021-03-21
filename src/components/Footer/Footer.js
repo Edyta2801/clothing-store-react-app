61b142efbe76cbeb251bf4a2be0c8b90ed8b70e1
Edyta2801 <edyta.szarowska@interia.pl>
2021-03-21 23:22:43 +0100

    add Footer with bug

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Center} from '@chakra-ui/react';
+
+const Footer = () => {
+  return (
+    <Center  bg="teal.500" h="100px" color="white">
+      2021 ClothesForYou All rights reserved
+    </Center>
+  );
+};
+export default Footer;
