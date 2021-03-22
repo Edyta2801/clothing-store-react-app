a4645ace95009855c3656a76631371175198baa4
Edyta2801 <edyta.szarowska@interia.pl>
2021-03-22 22:21:47 +0100

    add images for Single ProductPage

--- src/pages/SingleProductPage/SingleProductPage.js
+++ src/pages/SingleProductPage/SingleProductPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 // import {Wrap, WrapItem, Box, Heading, Button, Stack} from '@chakra-ui/react';
-import {Breadcrumb, BreadcrumbItem, BreadcrumbLink} from '@chakra-ui/react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Text,
+  Box,
+  Image,
+  Tabs,
+  TabPanels,
+  TabPanel,
+  TabList,
+  Tab,
+} from '@chakra-ui/react';
 
 const SingleProductPage = () => {
   return (
@@ -47,7 +59,65 @@ const SingleProductPage = () => {
           </BreadcrumbLink>
         </BreadcrumbItem>
       </Breadcrumb>
-      <div>Single Product Page </div>
+
+      <Box p={4} display={{md: 'flex'}}>
+        <Box flexShrink={0}>
+          <Tabs defaultIndex={1}>
+            <TabPanels>
+              <TabPanel>
+                <Image
+                  boxSize="200px"
+                  fit="cover"
+                  src="https://resizing.flixster.com/wTgvsiM8vNLhCcCH-6ovV8n5z5U=/
+                  300x300/v1.bjsyMDkxMzI5O2o7MTgyMDQ7MTIwMDsxMjAwOzkwMA"
+                />
+              </TabPanel>
+              <TabPanel>
+                <Image
+                  boxSize="200px"
+                  fit="cover"
+                  src="https://vignette.wikia.nocookie.net/naruto/images/2/21/
+                  Sasuke_Part_1.png/revision/latest?cb=20170716092103"
+                />
+              </TabPanel>
+            </TabPanels>
+            <TabList>
+              <Tab><Image
+                  boxSize="50px"
+                  fit="cover"
+                  src="https://resizing.flixster.com/wTgvsiM8vNLhCcCH-6ovV8n5z5U=/
+                  300x300/v1.bjsyMDkxMzI5O2o7MTgyMDQ7MTIwMDsxMjAwOzkwMA"
+                /></Tab>
+              <Tab>Sasuke</Tab>
+            </TabList>
+          </Tabs>
+        </Box>
+        <Box mt={{base: 4, md: 0}} ml={{md: 6}}>
+          <Text
+            fontWeight="bold"
+            textTransform="uppercase"
+            fontSize="sm"
+            letterSpacing="wide"
+            color="teal.600"
+          >
+            Marketing
+          </Text>
+          <Link
+            mt={1}
+            display="block"
+            fontSize="lg"
+            lineHeight="normal"
+            fontWeight="semibold"
+            href="#"
+          >
+            Finding customers for your new business
+          </Link>
+          <Text mt={2} color="gray.500">
+            Getting a new business off the ground is a lot of hard work. Here are five ideas you can
+            use to find your first customers.
+          </Text>
+        </Box>
+      </Box>
     </>
   );
 };
