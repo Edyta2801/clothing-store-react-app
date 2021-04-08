08c49048338d7ac92596ab8d1daffbf599727e76
Edyta2801 <edyta.szarowska@interia.pl>
2021-04-08 22:43:22 +0200

    add flex style for tabs

--- src/pages/SingleProductPage/SingleProductPage.js
+++ src/pages/SingleProductPage/SingleProductPage.js
@@ -13,8 +13,9 @@ import {
   TabPanel,
   TabList,
   Tab,
+  Stack,
+  Button,
 } from '@chakra-ui/react';
-
 
 import clothes from './images/clothes.jpg';
 import draft from './images/draft.jpg';
@@ -23,7 +24,6 @@ import sewing from './images/sewing.jpg';
 import manekin from './images/manekin.jpg';
 import show from './images/show.jpg';
 // import clothes from './images/clothes.jpg';
-
 
 const SingleProductPage = () => {
   return (
@@ -70,82 +70,69 @@ const SingleProductPage = () => {
         </BreadcrumbItem>
       </Breadcrumb>
 
-      <Box p={4} display={{md: 'flex'}}>
+      <Stack w="100%" align="flex-start" mt={{base: '10', md: '10'}} pl="2rem">
+        <Link to="/products" w="100%">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            borderRadius="8px"
+            py="4"
+            px="4"
+            lineHeight="1"
+            size="sm"
+          >
+            BACK TO PRODUCTS
+          </Button>
+        </Link>
+      </Stack>
+
+      <Box
+        p={4}
+        display={{md: 'flex'}}
+        align={{base: 'center'}}
+        // justify={{base: 'space-around', md: 'space-around', xl: 'space-between'}}
+        // direction={{base: 'column-reverse', md: 'row'}}
+        // wrap="no-wrap"
+      >
         <Box flexShrink={0}>
-          <Tabs defaultIndex={1}>
-            <TabPanels>
+          <Tabs
+            align={{base: 'center'}}
+            // isFitted
+            defaultIndex={0}
+          >
+            <TabPanels align="center">
               <TabPanel>
-                <Image
-                rounded="0.5rem"
-                  boxSize="430px"
-                  fit="cover"
-                  src={clothes}
-                />
+                <Image rounded="0.5rem" boxSize="430px" fit="cover" src={clothes} />
               </TabPanel>
               <TabPanel>
-                <Image
-                rounded="0.5rem"
-                  boxSize="430px"
-                  fit="cover"
-                  src={draft}
-                />
+                <Image rounded="0.5rem" boxSize="430px" fit="cover" src={draft} />
               </TabPanel>
               <TabPanel>
-                <Image
-                rounded="0.5rem"
-                  boxSize="430px"
-                  fit="cover"
-                  src={sewing}
-                />
+                <Image rounded="0.5rem" boxSize="430px" fit="cover" src={sewing} />
               </TabPanel>
               <TabPanel>
-                <Image
-                rounded="0.5rem"
-                  boxSize="430px"
-                  fit="cover"
-                  src={manekin}
-                />
+                <Image rounded="0.5rem" boxSize="430px" fit="cover" src={manekin} />
               </TabPanel>
               <TabPanel>
-                <Image
-                rounded="0.5rem"
-                  boxSize="430px"
-                  fit="cover"
-                  src={show}
-                />
+                <Image rounded="0.5rem" boxSize="430px" fit="cover" src={show} />
               </TabPanel>
             </TabPanels>
-            <TabList>
-              <Tab><Image
-              rounded="0.5rem"
-                  boxSize="60px"
-                  fit="cover"
-                  src={clothes}
-                /></Tab>
-              <Tab><Image
-              rounded="0.5rem"
-                  boxSize="60px"
-                  fit="cover"
-                  src={draft}
-                /></Tab>
-                 <Tab><Image
-                 rounded="0.5rem"
-                  boxSize="60px"
-                  fit="cover"
-                  src={sewing}
-                /></Tab>
-                 <Tab><Image
-                 rounded="0.5rem"
-                  boxSize="60px"
-                  fit="cover"
-                  src={manekin}
-                /></Tab>
-                 <Tab><Image
-                 rounded="0.5rem"
-                  boxSize="60px"
-                  fit="cover"
-                  src={show}
-                /></Tab>
+            <TabList justifyContent="center">
+              <Tab>
+                <Image rounded="0.5rem" boxSize="60px" fit="cover" src={clothes} />
+              </Tab>
+              <Tab>
+                <Image rounded="0.5rem" boxSize="60px" fit="cover" src={draft} />
+              </Tab>
+              <Tab>
+                <Image rounded="0.5rem" boxSize="60px" fit="cover" src={sewing} />
+              </Tab>
+              <Tab>
+                <Image rounded="0.5rem" boxSize="60px" fit="cover" src={manekin} />
+              </Tab>
+              <Tab>
+                <Image rounded="0.5rem" boxSize="60px" fit="cover" src={show} />
+              </Tab>
             </TabList>
           </Tabs>
         </Box>
